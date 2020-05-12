09f0b3f3835bb4cc2574048ed28dca8214870bf0
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-12 23:54:25 +0800

    基于开源项目SpringBoot+Vue二次开发交通时空大数据分析挖掘系统后端

--- vhrserver/vhr-service/src/main/java/org/javaboy/vhr/service/DepartmentService.java
+++ vhrserver/vhr-service/src/main/java/org/javaboy/vhr/service/DepartmentService.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 /**
- * @作者 江南一点雨
- * @公众号 江南一点雨
+ * @作者 深海鱼
+ * @公众号深海鱼
  * @微信号 a_java_boy
  * @GitHub https://github.com/lenve
  * @博客 http://wangsong.blog.csdn.net
--- vhrserver/vhr-service/src/main/java/org/javaboy/vhr/service/EmployeeService.java
+++ vhrserver/vhr-service/src/main/java/org/javaboy/vhr/service/EmployeeService.java
@@ -19,8 +19,8 @@ import java.util.List;
 import java.util.UUID;
 
 /**
- * @作者 江南一点雨
- * @公众号 江南一点雨
+ * @作者 深海鱼
+ * @公众号 深海鱼
  * @微信号 a_java_boy
  * @GitHub https://github.com/lenve
  * @博客 http://wangsong.blog.csdn.net
